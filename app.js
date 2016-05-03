94bfec75f75bedebfd8e62acbcd8443c3002a938
elliot irwin <elliot.irwin@pay4later.com>
2016-05-03 10:11:28 +0100

    added new /2/ endpoint

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ app.get('/', function (req, res) {
 	res.send('hello jenkins');
 });
 
+app.get('/2/', function (req, res) {
+	res.send('hi there');
+});
+
 app.listen(process.env.PORT || 5000);
 
 module.exports = app;
